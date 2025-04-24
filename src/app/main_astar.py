95981cf8335ed83e1c7b0fedc0e4c1e13c6848ff
Vitor Soller <vgsoller@gmail.com>
2025-04-23 22:47:02 -0300

    feat: separate in two mains

--- src/app/main_astar.py
+++ src/app/main_astar.py
@@ -35,7 +35,6 @@ def move(request: dict):
 
     path = board.get_path_to_closest_food(me)
     if len(path) > 0:
-        
         next_move = board.navigate_to(me.head, path[1])
     else:
         next_move = board.safe_random_move(me)
---
+++ src/app/main_heuristic.py
@@ -0,0 +1,55 @@
+import random
+from fastapi import FastAPI
+from mangum import Mangum
+from .entities.battlesnake import Battlesnake
+from .entities.board import Board
+
+app = FastAPI()
+
+# GET / - info about the snake (https://docs.battlesnake.com/api/requests/info)
+# POST /start - start the game (https://docs.battlesnake.com/api/requests/start)
+# POST /move - move the snake (https://docs.battlesnake.com/api/requests/move)
+# POST /end - end the game (https://docs.battlesnake.com/api/requests/end)
+
+@app.get("/")
+def read_root():
+    return {
+        "apiversion": "1",
+        "author": "SoBrRuMaSA",
+        "color": "#FFDE21",
+        "head": "ski",
+        "tail": "weight",
+        "version": "1.0.0"
+    }
+
+@app.post("/start") 
+def start():
+    return "ok"
+
+@app.post("/move")
+def move(request: dict):
+    print(request)
+
+    board = Board.from_json(request["board"])
+    me = Battlesnake.from_json(request["you"])
+
+    closest_food = board.get_closest_food(me)
+
+    if closest_food:
+        move = board.navigate_to(me.head, closest_food)
+        move = board.dodge_snake_body(me, move)
+    else:
+        move = board.safe_random_move(me)
+
+
+    response = {
+        "move": next_move,
+        "shout": f"I'm moving {next_move}!"
+    }
+    return response
+
+@app.post("/end")
+def end():
+    return "ok"
+
+handler = Mangum(app, lifespan="off")
