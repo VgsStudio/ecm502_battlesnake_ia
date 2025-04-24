bc56d2cc9788520efbc38b495eac53307bd8b0b0
Vitor Soller <vgsoller@gmail.com>
2025-04-23 22:50:12 -0300

    fix: change iac

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -22,78 +22,29 @@ class IacStack(Stack):
         self.project_name = os.environ.get("PROJECT_NAME")
         self.aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
 
-        lambda_fn = _lambda.Function(
-            self,
-            "BattleSnakeLambda",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            code=_lambda.Code.from_asset("../src"),
-            handler="app.main.handler",
-            timeout=Duration.seconds(15),
-        )
+        self.algorithms = ['star', 'heuristic']
 
-        lambda_url = lambda_fn.add_function_url(
-            auth_type=_lambda.FunctionUrlAuthType.NONE,
-        )
+        for algorithm in self.algorithms:
+            lambda_fn = _lambda.Function(
+                self,
+                f"BattleSnakeLambda{algorithm}",
+                runtime=_lambda.Runtime.PYTHON_3_9,
+                code=_lambda.Code.from_asset("../src"),
+                handler=f"app.main_{algorithm}.handler",
+                timeout=Duration.seconds(15),
+            )
 
-        password = self.project_name + "UserPassword7@"
+            lambda_url = lambda_fn.add_function_url(
+                auth_type=_lambda.FunctionUrlAuthType.NONE,
+            )
 
-        user = iam.User(self, self.project_name + "User",
-                        user_name=self.project_name + "User",
-                        password_reset_required=True,
-                        password=SecretValue.unsafe_plain_text(password)
+            CfnOutput(self, f"{self.project_name}{algorithm}Url",
+                      value=lambda_url.url,
+                      export_name=f"{self.project_name}{algorithm}UrlValue")    
+            
+            CfnOutput(self, f"{self.project_name}{algorithm}LambdaConsole",
+                        value="https://" + self.region + ".console.aws.amazon.com/lambda/home?region=" + self.region + "#/functions/" + lambda_fn.function_name + "?tab=code",
+                        export_name=f"{self.project_name}{algorithm}LambdaConsoleValue"
                         )
 
-        policy = iam.Policy(self, "Policy", statements=[
-            iam.PolicyStatement(
-                actions=["lambda:*"],
-                resources=[lambda_fn.function_arn]
-            )
-        ])
-
-        policy.add_statements(
-            iam.PolicyStatement(
-                actions=["logs:*"],
-                resources=["arn:aws:logs:*:*:*"]
-            )
-        )
-
-        policy.attach_to_user(user)
-
-        user.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("IAMUserChangePassword")
-        )
-
-        
-        alarm = lambda_fn.metric_invocations(
-            period=Duration.hours(6),
-        ).create_alarm(
-            self, self.project_name +"LambdaAlarm",
-            threshold=5000,
-            evaluation_periods=1,
-            comparison_operator=ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-        ) 
-        topic = Topic.from_topic_arn(self, self.project_name + "Topic", f"arn:aws:sns:{self.region}:{self.aws_account_id}:sns-battlesnake")
-        sns_action = SnsAction(topic)
-
-        alarm.add_alarm_action(sns_action)
-
-        CfnOutput(self, self.project_name + "Url",
-                  value=lambda_url.url,
-                  export_name= self.project_name + 'UrlValue')    
-
-        CfnOutput(self, self.project_name + "UserOutput",
-                  value=user.user_name,
-                  export_name= self.project_name + 'UserValue'
-                  )
-
-        CfnOutput(self, self.project_name + "FirstTimeUserPassword",
-                  value=password,
-                  export_name= self.project_name + 'FirstTimeUserPasswordValue'
-                  )    
-        
-        CfnOutput(self, self.project_name + "LambdaConsole",
-                    value="https://" + self.region + ".console.aws.amazon.com/lambda/home?region=" + self.region + "#/functions/" + lambda_fn.function_name + "?tab=code",
-                    export_name= self.project_name + 'LambdaConsoleValue'
-                    )
-        
-
+       
